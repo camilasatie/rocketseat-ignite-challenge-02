6525756fa84094cfb70c4ef7887ac9f1193cd5d8
Camila <camilasatie.dev@gmail.com>
2021-04-04 09:39:18 -0300

    feat: show amount of products

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,8 +7,10 @@ import { Container, Cart } from './styles';
 import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+  const { cart } = useCart();
+  const cartSize = cart.length;
+
+  console.log(cartSize)
 
   return (
     <Container>
@@ -20,7 +22,7 @@ const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
